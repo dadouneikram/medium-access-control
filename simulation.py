eb7c0f541af5572b91c7d9b2cc09a5c2ef94dd6b
Ikram Dadoune <dadouneikram10@gmail.com>
2026-04-27 16:19:03 +0200

    readme et methode simuler

--- simulation.py
+++ simulation.py
@@ -5,8 +5,8 @@ class Station:
     def __init__(self, id, K):
         self.id = id
         self.K = K              
-        self.file = 0           
-        self.etat = 1           
+        self.file = 0          
+        self.etat = 1          
         self.prochaine_tentative = None 
         
         self.paquets_perdus = 0
@@ -21,5 +21,20 @@ class Station:
             return False
 
     def calculer_backoff(self, t_actuel, tau):
-        delai = random.expovariate(1 / (pow(2, self.etat) * tau))
+        moyenne = (2**self.etat) * tau
+        delai = random.expovariate(1.0 / moyenne)
         self.prochaine_tentative = t_actuel + delai
+
+def simuler(N, K, lambd, tau, temps_max):
+    t = 0
+    stations = [Station(i, K) for i in range(N)]
+    
+    prochaines_arrivees = [random.expovariate(lambd) for _ in range(N)]
+    
+    total_reussis = 0
+    historique_debit = []
+
+    while t < temps_max:
+      t += 0.1 
+        
+    return total_reussis / t
